8983a4bf6c0d3b40a82b8d4525f6287568bb9e97
Sina <sinaderik7@gmail.com>
2024-10-30 21:58:04 +0330

    inline curried function is created

--- curryFunction/app.js
+++ curryFunction/app.js
@@ -1,12 +1,20 @@
 // In JavaScript, currying is a technique where a function with multiple arguments is transformed into a series of functions that each take a single argument.
 
-function makeSandwich(ingredient1){
-    return (ingredient2)=>{
-        return(ingredient3)=>{
-            return `${ingredient1} - ${ingredient2} - ${ingredient3}`
-        }
-    }
+function makeSandwich(ingredient1) {
+  return (ingredient2) => {
+    return (ingredient3) => {
+      return `${ingredient1} - ${ingredient2} - ${ingredient3}`;
+    };
+  };
 }
 
-const mySandwich=makeSandwich("onion")("burger")("tomato")
-console.log(mySandwich)
+const mySandwich = makeSandwich("onion")("burger")("tomato");
+// console.log(mySandwich)
+
+const makeSan = (ingredient1) => (ingredient2) => (ingredient3) =>
+  `${ingredient1} - ${ingredient2} - ${ingredient3}`;
+
+const mySan=makeSan("in 1")("in 2")("in 3")
+// console.log(mySan)
+
+
