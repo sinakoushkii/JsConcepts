18456d873c9c9c852c3fe05f6f0f3205ede14284
Sina <sinaderik7@gmail.com>
2024-10-30 23:41:35 +0330

    another example of curry function is implemented

--- curryFunction/app.js
+++ curryFunction/app.js
@@ -19,5 +19,21 @@ const mySan = makeSan("in 1")("in 2")("in 3");
 
 // Partialy apllied functions are a common use of curring
 const carriedMultiply = (x) => (y) => x * y;
-const timesFour=carriedMultiply(4)
-console.log(timesFour(5))
+const timesFour = carriedMultiply(4);
+console.log(timesFour(5));
+
+function add(a, b, c) {
+  return a + b + c;
+}
+
+function curry(fn) {
+  return function curriedFn(...args) {
+    if (fn.length !== args.length) {
+      return curriedFn.bind(null, ...args);
+    }
+    
+    return fn.apply(null, args);
+  };
+}
+const result = curry(add);
+console.log(result(10,20)(30))
